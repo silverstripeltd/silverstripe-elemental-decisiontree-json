2de4c452f85aea4c299a0e4a1c30f63fcb2dd947
Mohamed Alsharaf <mohamed.alsharaf@silverstripe.com>
2025-05-06 10:18:38 +1200

    Add support for CMS 6

--- src/DecisionTree/Output/BlockExtension.php
+++ src/DecisionTree/Output/BlockExtension.php
@@ -4,7 +4,9 @@ namespace Silverstripe\Elemental\DecisionTree\Output;
 
 use DNADesign\SilverStripeElementalDecisionTree\Model\DecisionTreeStep;
 use JsonException;
+use Psr\SimpleCache\CacheInterface;
 use SilverStripe\Core\Extension;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\ORM\SS_List;
 use SilverStripe\View\Parsers\ShortcodeParser;
 
@@ -13,15 +15,63 @@ use SilverStripe\View\Parsers\ShortcodeParser;
  */
 class BlockExtension extends Extension
 {
+    protected const STRING CACHE_KEY = 'output';
+
     /**
      * Holds collection of tree data while JSON is created
      */
     protected array $treeData = [];
 
     /**
+     * After publish refresh the cache
+     */
+    public function onAfterPublish(): void
+    {
+        $this->getCacheInstance()->clear();
+        $this->getCachedJson();
+    }
+
+    /**
+     * On delete clear the cache
+     */
+    public function onBeforeDelete(): void
+    {
+        $this->getCacheInstance()->clear();
+    }
+
+    /**
      * Get all tree data in JSON string for front ends implementation.
      */
-    public function getJson(): string
+    public function getJson(bool $useCache = false): string
+    {
+        return $useCache
+            ? $this->getCachedJson()
+            : $this->getFreshJson();
+    }
+
+    /**
+     * Get tree data from cache
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    protected function getCachedJson(): string
+    {
+        $cache = $this->getCacheInstance();
+
+        if (!$cache->has(self::CACHE_KEY)) {
+            $data = $this->getFreshJson();
+            $cache->set(self::CACHE_KEY, $data);
+        }
+
+        return $cache->get(self::CACHE_KEY);
+    }
+
+    /**
+     * Get tree data from source
+     *
+     * @throws \Exception
+     */
+    protected function getFreshJson(): string
     {
         try {
             return base64_encode(json_encode(array_merge(
@@ -34,6 +84,16 @@ class BlockExtension extends Extension
         } catch (JsonException) {
             return '{}';
         }
+    }
+
+    /**
+     * Get instance of the cache
+     *
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
+    protected function getCacheInstance(): CacheInterface
+    {
+        return Injector::inst()->get(CacheInterface::class . '.decisiontree-output');
     }
 
     /**
